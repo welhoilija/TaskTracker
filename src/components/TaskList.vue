a9fd2ffc13dc0c00039586edad26a591bb34e4d8
Tuomas Kangas <tkankas@gmail.com>
2023-12-14 17:14:53 +0200

    add favicon

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,25 +1,28 @@
 <template>
-    <ul>
-        <li v-for="task in tasksToDisplay" :key="task.id" :style="{ paddingLeft: depth * 10 + 'px' }">
-            <TaskCard 
-                :task="task"
-                :allowDescendants="depth < 6"
-                @taskCreated="handleTaskCreated"
-                @taskDeleted="handleTaskDeleted"
-                @taskStateChanged="handleTaskStateChanged"
-            ></TaskCard>
-            <TaskList 
-                v-if="collectSubTasks(task.id).length > 0"
-                :tasks="tasks" 
-                :subTasks="collectSubTasks(task.id)"
-                :depth="depth + 1" 
-                @taskCreated="handleTaskCreated" 
-                @taskDeleted="handleTaskDeleted"
-                @taskStateChanged="handleTaskStateChanged"
-            />
-
-        </li>
-    </ul>
+  <ul>
+    <li
+      v-for="task in tasksToDisplay"
+      :key="task.id"
+      :style="{ paddingLeft: depth * 10 + 'px' }"
+    >
+      <TaskCard
+        :task="task"
+        :allowDescendants="depth < 6"
+        @taskCreated="handleTaskCreated"
+        @taskDeleted="handleTaskDeleted"
+        @taskStateChanged="handleTaskStateChanged"
+      ></TaskCard>
+      <TaskList
+        v-if="collectSubTasks(task.id).length > 0"
+        :tasks="tasks"
+        :subTasks="collectSubTasks(task.id)"
+        :depth="depth + 1"
+        @taskCreated="handleTaskCreated"
+        @taskDeleted="handleTaskDeleted"
+        @taskStateChanged="handleTaskStateChanged"
+      />
+    </li>
+  </ul>
 </template>
 
 <script lang="ts" src="./TaskList.tsx" />
